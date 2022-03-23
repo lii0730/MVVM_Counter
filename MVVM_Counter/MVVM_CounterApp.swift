fda3cd49d038383a7d9004ab2ae99dec8ddf8a03
LeeHsss <lii0730@icloud.com>
2022-03-23 14:36:52 +0900

    Initial Commit

---
+++ MVVM_Counter/MVVM_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_CounterApp.swift
+//  MVVM_Counter
+//
+//  Created by LeeHsss on 2022/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
